8551e5383b6f0be0a7a63030b657de9864de3fd7
Ben Parisot <me@benparisot.com>
2019-03-13 15:14:14 -0700

    make account and track-debate pages, update index.html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import candidates from '../data/candidates.js';
-import loadCandidates from './candidate-list-component.js';
-import startDebate from './debate-start-component.js';
-import scoreCandidates from './score-plus-minus-component.js';
-import sortCandidatesByDebateScore from './candidates-sort-component.js';
+
 import loadHeader from './make-header-component.js';
-import { auth, candidatesListByUserRef } from './firebase.js';
+import { auth } from './firebase.js';
 
 loadHeader();
-loadCandidates(candidates);
-startDebate(candidates);
-scoreCandidates(candidates);
 
 const endDebateButtonNode = document.getElementById('end-debate');
 
---
+++ src/track-debate.js
@@ -0,0 +1,22 @@
+import candidates from '../data/candidates.js';
+import loadCandidates from './candidate-list-component.js';
+import startDebate from './debate-start-component.js';
+import scoreCandidates from './score-plus-minus-component.js';
+import sortCandidatesByDebateScore from './candidates-sort-component.js';
+import loadHeader from './make-header-component.js';
+import { auth, candidatesListByUserRef } from './firebase.js';
+
+loadHeader();
+loadCandidates(candidates);
+startDebate(candidates);
+scoreCandidates(candidates);
+
+const endDebateButtonNode = document.getElementById('end-debate');
+
+endDebateButtonNode.addEventListener('click', () => {
+    const sortedCandidates = sortCandidatesByDebateScore(candidates);
+    loadCandidates(sortedCandidates);
+    const userID = auth.currentUser.uid;
+    candidatesListByUserRef.child(userID)
+        .set(sortedCandidates);
+});
