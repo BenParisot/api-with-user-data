17d16889b41a47c3266bb163f874156fb2afe85b
Ben Parisot <me@benparisot.com>
2019-03-14 14:59:36 -0700

    get array of favorited articles from fbdb by userid

--- src/account.js
+++ src/account.js
@@ -2,8 +2,13 @@ import { auth, favoriteArticlesByUserRef } from './firebase.js';
 import loadHeader from './make-header-component.js';
 
 loadHeader();
-console.log('hello');
+
 auth.onAuthStateChanged(user => {
     const userID = auth.currentUser.uid;
-    console.log(userID);
+    const favoriteArticles = favoriteArticlesByUserRef.child(userID);
+    favoriteArticles.once('value')
+        .then(snapshot => {
+            const value = snapshot.val();
+            console.log(value);
+        });
 });
