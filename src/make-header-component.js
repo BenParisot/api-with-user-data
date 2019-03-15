8e0ffe0ca82cde2a2eebf48cf3c63fa12404f5d1
Ben Parisot <me@benparisot.com>
2019-03-15 10:14:35 -0700

    begin adding content and styling index.html

--- src/make-header-component.js
+++ src/make-header-component.js
@@ -1,24 +1,11 @@
 import { auth } from './firebase.js';
 
-export function makeStaticHeader() {
-    const html = `
-    <section>
-        <img src="https://static.thenounproject.com/png/678952-200.png" alt="debate icon">
-        <h1>Track Your Candidate</h1>
-    </section>
-    `;
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
-
 export function makeProfileHeader(user) {
     const avatar = user.photoURL || 'https://exelord.com/ember-initials/images/default-d5f51047d8bd6327ec4a74361a7aae7f.jpg';
     const html = `
     <section id="profile-display">
         <img src="${avatar}" alt="user avatar">
         <h2>${user.displayName}</h2>
-        <a href="account.html">Your Account</a>
         <button>Sign Out</button>
     </section>`;
     const template = document.createElement('template');
@@ -29,9 +16,6 @@ export function makeProfileHeader(user) {
 const headerNode = document.getElementById('header');
 
 export default function loadHeader(options) {
-    const staticHeader = makeStaticHeader();
-    headerNode.appendChild(staticHeader);
-    
     if(options && options.authSkip) {
         return;
     }
--- test/make-header.test.js
+++ test/make-header.test.js
@@ -1,18 +1,5 @@
-import { makeStaticHeader, makeProfileHeader } from '../src/make-header-component.js';
+import { makeProfileHeader } from '../src/make-header-component.js';
 const test = QUnit.test;
-
-test('make static header from template', assert => {
-    const expected = `
-    <section>
-        <img src="https://static.thenounproject.com/png/678952-200.png" alt="debate icon">
-        <h1>Track Your Candidate</h1>
-    </section>
-    `;
-
-    const result = makeStaticHeader();
-
-    assert.htmlEqual(result, expected);
-});
 
 test('make dynamic profile section from template', assert => {
     const user = {
@@ -23,7 +10,6 @@ test('make dynamic profile section from template', assert => {
     <section id="profile-display">
         <img src="https://exelord.com/ember-initials/images/default-d5f51047d8bd6327ec4a74361a7aae7f.jpg" alt="user avatar">
         <h2>Display Name</h2>
-        <a href="account.html">Your Account</a>
         <button>Sign Out</button>
     </section>`;
 
